01ffe4c75d6ba6a63c97f2fbe16c4ec384c70bec
Curtis Chambers <curtis.chambers@live.com>
2022-12-01 08:01:59 -0600

    finished part one of day 1.

--- 1/a.ts
+++ 1/a.ts
@@ -1,1 +1,25 @@
-console.log("day 1");
+export const x = "";
+
+const input = await Deno.readTextFile("./input.txt");
+
+const elfArr = input
+	.split("\n\n")
+	.map((x) => x.split("\n").map((x) => parseInt(x)));
+
+let highest = 0;
+let acc = 0;
+
+elfArr[0].forEach((x) => {
+	if (!Number.isNaN(x)) {
+		acc += x;
+	} else if (Number.isNaN(x)) {
+		if (acc > highest) {
+			highest = acc;
+			acc = 0;
+		} else {
+			acc = 0;
+		}
+	}
+});
+
+console.log(highest);
