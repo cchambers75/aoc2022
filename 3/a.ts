0ea548e7c31539566f34ae55300c0bf720a2b010
Curtis Chambers <curtis.chambers@live.com>
2022-12-03 09:24:32 -0600

    finished part one of day 3

---
+++ 3/a.ts
@@ -0,0 +1,77 @@
+export const exp = "";
+
+const input = await Deno.readTextFile("./input.txt");
+
+const sack = input.split("\n");
+
+let total = 0;
+
+const prio = new Map();
+
+prio.set("a", 1);
+prio.set("b", 2);
+prio.set("c", 3);
+prio.set("d", 4);
+prio.set("e", 5);
+prio.set("f", 6);
+prio.set("g", 7);
+prio.set("h", 8);
+prio.set("i", 9);
+prio.set("j", 10);
+prio.set("k", 11);
+prio.set("l", 12);
+prio.set("m", 13);
+prio.set("n", 14);
+prio.set("o", 15);
+prio.set("p", 16);
+prio.set("q", 17);
+prio.set("r", 18);
+prio.set("s", 19);
+prio.set("t", 20);
+prio.set("u", 21);
+prio.set("v", 22);
+prio.set("w", 23);
+prio.set("x", 24);
+prio.set("y", 25);
+prio.set("z", 26);
+prio.set("A", 27);
+prio.set("B", 28);
+prio.set("C", 29);
+prio.set("D", 30);
+prio.set("E", 31);
+prio.set("F", 32);
+prio.set("G", 33);
+prio.set("H", 34);
+prio.set("I", 35);
+prio.set("J", 36);
+prio.set("K", 37);
+prio.set("L", 38);
+prio.set("M", 39);
+prio.set("N", 40);
+prio.set("O", 41);
+prio.set("P", 42);
+prio.set("Q", 43);
+prio.set("R", 44);
+prio.set("S", 45);
+prio.set("T", 46);
+prio.set("U", 47);
+prio.set("V", 48);
+prio.set("W", 49);
+prio.set("X", 50);
+prio.set("Y", 51);
+prio.set("Z", 52);
+
+sack.map((x) => {
+	const letters = x.split("");
+	const first = letters.slice(0, letters.length / 2);
+	const second = letters.slice(letters.length / 2);
+	const letterSet = new Set();
+	first.forEach((x) => {
+		if (second.includes(x) && !letterSet.has(x)) {
+			letterSet.add(x);
+			total += prio.get(x);
+		}
+	});
+});
+
+console.log(total);
